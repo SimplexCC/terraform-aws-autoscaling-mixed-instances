d4767d59c4bb324640e0845ba70ae24a6aea8596
Hans Gunawan <hans.gunawan@hennge.com>
2021-02-12 13:50:02 +0900

    Use dynamic for initial lifecycle hook block

--- main.tf
+++ main.tf
@@ -61,7 +61,6 @@ resource "aws_launch_template" "this" {
 # Autoscaling group
 ####################
 resource "aws_autoscaling_group" "this" {
-  count = var.create_asg && false == var.create_asg_with_initial_lifecycle_hook ? 1 : 0
 
   name_prefix = "${join(
     "-",
@@ -122,96 +121,17 @@ resource "aws_autoscaling_group" "this" {
     }
   }
 
-  tags = concat(
-    [
-      {
-        key                 = "Name"
-        value               = var.name
-        propagate_at_launch = true
-      },
-    ],
-    var.tags,
-    local.tags_asg_format,
-  )
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-################################################
-# Autoscaling group with initial lifecycle hook
-################################################
-resource "aws_autoscaling_group" "this_with_initial_lifecycle_hook" {
-  count = var.create_asg && var.create_asg_with_initial_lifecycle_hook ? 1 : 0
-
-  name_prefix = "${join(
-    "-",
-    compact(
-      [
-        coalesce(var.asg_name, var.name),
-        var.recreate_asg_when_lt_changes ? element(concat(random_pet.asg_name.*.id, [""]), 0) : "",
-      ],
-    ),
-  )}-"
-  vpc_zone_identifier = var.vpc_zone_identifier
-  max_size            = var.max_size
-  min_size            = var.min_size
-  desired_capacity    = var.desired_capacity
-
-  load_balancers            = var.load_balancers
-  health_check_grace_period = var.health_check_grace_period
-  health_check_type         = var.health_check_type
-
-  min_elb_capacity          = var.min_elb_capacity
-  wait_for_elb_capacity     = var.wait_for_elb_capacity
-  target_group_arns         = var.target_group_arns
-  default_cooldown          = var.default_cooldown
-  force_delete              = var.force_delete
-  termination_policies      = var.termination_policies
-  suspended_processes       = var.suspended_processes
-  placement_group           = var.placement_group
-  enabled_metrics           = var.enabled_metrics
-  metrics_granularity       = var.metrics_granularity
-  wait_for_capacity_timeout = var.wait_for_capacity_timeout
-  protect_from_scale_in     = var.protect_from_scale_in
-  service_linked_role_arn   = var.service_linked_role_arn
-  max_instance_lifetime     = var.max_instance_lifetime
-  capacity_rebalance        = var.capacity_rebalance
-
-  mixed_instances_policy {
-    launch_template {
-      launch_template_specification {
-        launch_template_id = var.create_lt ? element(concat(aws_launch_template.this.*.id, [""]), 0) : var.launch_template
-        version            = "$Latest"
-      }
-
-      dynamic "override" {
-        for_each = var.instance_types
-        content {
-          instance_type     = lookup(override.value, "instance_type", null)
-          weighted_capacity = lookup(override.value, "weighted_capacity", null)
-        }
-      }
+  dynamic "initial_lifecycle_hook" {
+    for_each = var.create_asg && var.create_asg_with_initial_lifecycle_hook ? [1] : []
+    content {
+      name                    = var.initial_lifecycle_hook_name
+      lifecycle_transition    = var.initial_lifecycle_hook_lifecycle_transition
+      notification_metadata   = var.initial_lifecycle_hook_notification_metadata
+      heartbeat_timeout       = var.initial_lifecycle_hook_heartbeat_timeout
+      notification_target_arn = var.initial_lifecycle_hook_notification_target_arn
+      role_arn                = var.initial_lifecycle_hook_role_arn
+      default_result          = var.initial_lifecycle_hook_default_result
     }
-
-    instances_distribution {
-      on_demand_base_capacity                  = var.on_demand_base_capacity
-      on_demand_percentage_above_base_capacity = var.on_demand_percentage_above_base_capacity
-      spot_allocation_strategy                 = var.spot_allocation_strategy
-      spot_instance_pools                      = var.spot_allocation_strategy == "lowest-price" ? var.spot_instance_pools : null
-      spot_max_price                           = var.spot_price
-    }
-  }
-
-  initial_lifecycle_hook {
-    name                    = var.initial_lifecycle_hook_name
-    lifecycle_transition    = var.initial_lifecycle_hook_lifecycle_transition
-    notification_metadata   = var.initial_lifecycle_hook_notification_metadata
-    heartbeat_timeout       = var.initial_lifecycle_hook_heartbeat_timeout
-    notification_target_arn = var.initial_lifecycle_hook_notification_target_arn
-    role_arn                = var.initial_lifecycle_hook_role_arn
-    default_result          = var.initial_lifecycle_hook_default_result
   }
 
   tags = concat(
